e253fae75d2d544594a76605e49080de2f862936
CeciBlanco <mceci.blanco@hotmail.com>
2023-09-19 16:03:10 -0300

    Agregado collision (clase)

--- Mi proyecto 2D/Assets/BasicMovement.cs
+++ Mi proyecto 2D/Assets/BasicMovement.cs
@@ -7,7 +7,13 @@ public class BasicMovement : MonoBehaviour
     public float horizontal;
     public float vertical;
     public float walkingSpeed;
+    public Rigidbody2D cuerpo;
 
+
+    private void Start()
+    {
+        cuerpo = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,8 +23,8 @@ public class BasicMovement : MonoBehaviour
 
         Vector2 movimiento = ((Vector2.right * horizontal) + (Vector2.up * vertical)) * walkingSpeed;
 
-        transform.position += new Vector3(movimiento.x, movimiento.y, 0) * Time.deltaTime;
-
+        //transform.position += new Vector3(movimiento.x, movimiento.y, 0) * Time.deltaTime;
+        cuerpo.velocity = new Vector2(movimiento.x,movimiento.y) * Time.deltaTime;
 
     }
 }
